ba26514f517763233e219174f6f0d0a1d3fcbfe1
Elham Karamian <elhamkaramian@MacBook-Pro-de-Elham.local>
2023-06-06 11:33:51 +0200

    add static files and create model comment

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'active', 'price']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'active', 'stars', 'author', 'recommend']
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -7,7 +8,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     active = models.BooleanField(default=True)
-
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
@@ -16,3 +16,25 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'perfect'),
+    ]
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name='comments', )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    active = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body
+
